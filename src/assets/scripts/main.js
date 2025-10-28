bbc576f9cc0311aba8d703bf14d34f8e125ab138
joadelvia <joadelvia@iesjacaranda.es>
2025-10-28 06:08:40 +0100

    Avanzando

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,12 +5,39 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/swiper-bundle.css';
+import 'swiper/swiper.css';
+
+
 
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
++(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+})();
+
+AOS.init({
+  duration: 800,
+  once: true
+})
+
+const swiper = new Swiper('.swiper', {
+  autoplay: { delay: 3000, disableOnInteraction: false },
+  pagination: { el: '.swiper-pagination' },
+  direction: 'horizontal',
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  modules: [Navigation, Pagination, Autoplay]
+});
